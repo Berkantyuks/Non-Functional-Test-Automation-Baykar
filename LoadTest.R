0542e865e249060dc0c3f6fe0338016743fb6cdb
Berkant Yüksektepe <61010367+Berkantyuks@users.noreply.github.com>
2022-08-21 16:51:52 +0300

    Some code optimizations and new result file.

--- LoadTest.R
+++ LoadTest.R
@@ -35,7 +35,7 @@ print(results)
 ## Create pdf file by test results
 CreatePDF.LoadTest <- function(results)
 {
-  pdf(file= paste("results\\",currentDate,"test_result.pdf"), width = 10, height = 8)
+  pdf(file= paste("results\\",currentDate,"test_result.pdf"), width = 11, height = 9)
   
   # create a 2X2 grid
   par( mfrow= c(2,2) )
@@ -48,7 +48,7 @@ CreatePDF.LoadTest <- function(results)
   
   ac <- annotation_custom(rasterGrob(test_mark, 
                                      x = 1, 
-                                     y=0.85, 
+                                     y=0.89, 
                                      hjust = 1, 
                                      width= .12, 
                                      interpolate=TRUE))
@@ -60,11 +60,11 @@ CreatePDF.LoadTest <- function(results)
                            ncol = 2, nrow = 2)
   
   out <- annotate_figure(grid_graphs,
-                  bottom = text_grob(paste(" Test Executed by: \n Berkant Yüksektepe \n"), color = "blue",
-                                     hjust = 1, x = 1, face = "italic", size = 10),
+                  bottom = text_grob(paste("This file auto generated and executed by: Berkant Yüksektepe. Test Class Non-Functional Used in plots. This page involves only load and performance tests, \n for more information please visit project repo: https://github.com/Berkantyuks/Non-Functional-Test-Automation-Baykar\n"), color = "red",
+                                    face = "italic", size = 10),
                   left = paste(currentDate),
                   right = paste(currentDate),
-                  top = text_grob(paste("Load Test: ", test_url, "\n"), color = "black", face = "bold", size = 14))
+                  top = text_grob(paste("\n Load Test: ", test_url, "\n"), color = "black", face = "bold", size = 14))
   print(out)
   dev.off()
 }
