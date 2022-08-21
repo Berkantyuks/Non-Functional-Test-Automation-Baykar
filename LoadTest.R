11640135f1f3a9b2b1bd3f2ac7dbe708abe967d2
Berkant Yüksektepe <61010367+Berkantyuks@users.noreply.github.com>
2022-08-21 17:20:05 +0300

    New Results Added.

--- LoadTest.R
+++ LoadTest.R
@@ -19,13 +19,16 @@ library(png)
 
 test_url <- c("https://kariyer.baykartech.com")
 test_mark <- readPNG("img/tcnf-test-logo.png")
-currentDate <- Sys.Date()
+currentDate <- Sys.time()
+
+thr = 7
+loops = 10
 
 # Run loadtest
 results <- loadtest(url = test_url,
                     method = "GET",
-                    threads = 7,
-                    loops = 10,
+                    threads = thr,
+                    loops = loops,
                     delay_per_request=100)
 
 # Print Results
@@ -64,7 +67,7 @@ CreatePDF.LoadTest <- function(results)
                                     face = "italic", size = 10),
                   left = paste(currentDate),
                   right = paste(currentDate),
-                  top = text_grob(paste("\n Load Test: ", test_url, "\n"), color = "black", face = "bold", size = 14))
+                  top = text_grob(paste("\n Load Test: ",test_url," (THR: ",thr," LPS: ",loops,")","\n"), color = "black", face = "bold", size = 14))
   print(out)
   dev.off()
 }
